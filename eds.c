9a9d08aea752ccbd264d9b9f6091eb2851bb8df1
renato <r.seiji0@gmail.com>
2017-09-25 11:58:57 -0300

    correcoes nas variaveis passadas errado

--- eds.c
+++ eds.c
@@ -14,49 +14,47 @@
 #define dr 1.35
 #define r 0.395
 #define K 1.219
-#define V 50
+#define V 50 / 3.6
+#define to_radians M_PI / 180
+#define to_degree 180/M_PI
 
 // calcula o angulo da roda dianteira interna
 float inner_steering_angle(int steering_angle)
 {
-    float angle, aux;    
-    
-    aux = M_PI / 180;
-    angle = atan((L * tan(steering_angle * aux))/(L - ((K/2) * tan(steering_angle * aux))));
+    float angle;    
+    angle = atan((L * tan(steering_angle * to_radians))/(L - ((K/2) * tan(steering_angle * to_radians)))) * to_degree;
     return angle;
 }
 
 // calcula o angulo da roda dianteira externa
 float outer_steering_angle(int steering_angle)
 {
-    float angle, aux;
-    aux = M_PI / 180;
-    angle = atan((L * tan(steering_angle * aux))/(L + ((K/2) * tan(steering_angle * aux))));
+    float angle;
+    
+    angle = atan((L * tan(steering_angle * to_radians))/(L + ((K/2) * tan(steering_angle * to_radians))))*to_degree;
     return angle;
 }
 
 // calcula o raio de giro do veiculo
-void wheel_turning_radius(float R1, float R2, float R3, float R4, float inner_angle, float outer_angle, float steering_angle)
+void wheel_turning_radius(float *R1, float *R2, float *R3, float *R4, float inner_angle, float outer_angle, float steering_angle)
 {
-    float aux;
-    aux = M_PI / 180;
-    R1 = L/sin(inner_angle * aux);
-    R2 = L/sin(outer_angle * aux);
-    R3 = (L/tan(steering_angle * aux))-(dr/2);
-    R4 = (L/tan(steering_angle * aux))+(dr/2);
+    *R1 = L/sin(inner_angle * to_radians);
+    *R2 = L/sin(outer_angle * to_radians);
+    *R3 = (L/tan(steering_angle * to_radians))-(dr/2);
+    *R4 = (L/tan(steering_angle * to_radians))+(dr/2);
 }
 
-float cg_radius(float Rcg, float R3)
+float cg_radius(float *Rcg, float *R3)
 {
-    Rcg = sqrt((R3 + pow(dr/2, 2)) + pow(lr, 2));
+    *Rcg = sqrt((*R3 + pow(dr/2, 2)) + pow(lr, 2));
 }
 
-void wheel_speeds(float W1, float W2, float W3, float W4, float R1, float R2, float R3, float R4, float Rcg)
+void wheel_speeds(float *W1, float *W2, float *W3, float *W4, float *R1, float *R2, float *R3, float *R4, float *Rcg)
 {
-    W1 = (V * R1) / (Rcg * r);
-    W2 = (V * R2) / (Rcg * r);
-    W3 = (V * R3) / (Rcg * r);
-    W4 = (V * R4) / (Rcg * r);
+    *W1 = (V * *R1) / (*Rcg * r);
+    *W2 = (V * *R2) / (*Rcg * r);
+    *W3 = (V * *R3) / (*Rcg * r);
+    *W4 = (V * *R4) / (*Rcg * r);
 }
 
 int main(void)
@@ -76,13 +74,12 @@ int main(void)
     for(steering_angle = 0; steering_angle < 15; steering_angle++)
     {
         inner_angle = inner_steering_angle(steering_angle);
-        outer_angle = outer_steering_angle(steering_angle);
-        printf("%f\n", outer_angle);
-        wheel_turning_radius(R1, R2, R3, R4, inner_angle, outer_angle, steering_angle);
-        cg_radius(Rcg, R3);
-        wheel_speeds(W1_speed[steering_angle], W2_speed[steering_angle], W3_speed[steering_angle], W4_speed[steering_angle], R1, R2, R3, R4, Rcg);
+        outer_angle = outer_steering_angle(steering_angle);        
+        wheel_turning_radius(&R1, &R2, &R3, &R4, inner_angle, outer_angle, steering_angle);
+        cg_radius(&Rcg, &R3);
+        wheel_speeds(&W1_speed[steering_angle], &W2_speed[steering_angle], &W3_speed[steering_angle], &W4_speed[steering_angle], &R1, &R2, &R3, &R4, &Rcg);
         
-        // printf("RPM wheel 1 = %f | RPM wheel 2 = %f \n", W1_speed[steering_angle], W2_speed[steering_angle]);
+        printf("RPM wheel 1 = %f | RPM wheel 2 = %f \n", W1_speed[steering_angle], W2_speed[steering_angle]);
     }
     
 }
